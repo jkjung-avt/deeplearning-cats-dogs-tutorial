096e5dab3126e5f550e42501d4a1ec71133f66b0
JK Jung <jkjung13@gmail.com>
2017-08-14 15:55:25 +0800

    Open the mean.binaryproto file as binary, specifically

--- code/make_predictions_1.py
+++ code/make_predictions_1.py
@@ -51,7 +51,7 @@ Reading mean image, caffe model and its weights
 '''
 #Read mean image
 mean_blob = caffe_pb2.BlobProto()
-with open('/home/nvidia/project/deeplearning-cats-dogs-tutorial/input/mean.binaryproto') as f:
+with open('/home/nvidia/project/deeplearning-cats-dogs-tutorial/input/mean.binaryproto', 'rb') as f:
     mean_blob.ParseFromString(f.read())
 mean_array = np.asarray(mean_blob.data, dtype=np.float32).reshape(
     (mean_blob.channels, mean_blob.height, mean_blob.width))
